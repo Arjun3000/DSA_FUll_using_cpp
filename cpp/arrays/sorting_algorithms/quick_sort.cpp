d12ece1acfcc27f59b79703aaee239c90eb4555c
Arjun3000 <arjunsharma.cse2023.gna@gmail.com>
2025-07-01 10:12:27 +0530

    all

---
+++ cpp/arrays/sorting_algorithms/quick_sort.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+int partition(int arr[],int s,int e)
+{
+    int p = arr[0];
+    int i;
+    int count = 0;
+    for(i=s+1;i<=e;i++)
+    {
+        if(arr[i]<= p)
+        {
+            count++;
+        }
+    }
+    int pi = s+count;
+    swap(arr[pi],arr[s]);
+
+    int i = s;
+    int j = e;
+    while(i<pi&&j>pi)
+    {
+         while(arr[i]<arr[pi])
+         {
+            i++;
+         }
+         while(arr[j]<arr[pi])
+         {
+            j--;
+         }
+         swap(arr[i++],arr[j--]);
+    }
+}
+void quick(int arr[],int s,int e)
+{
+   int p = partition(arr,s,e);
+   quick(arr,s,p-1);
+   quick(arr,p+1,s);
+}
+int main()
+{
+    int i,n=5;
+    int arr[n];
+    cout<<"enter elements in array : "<<endl;
+    for(i=0;i<n;i++)
+    {
+        cin>>arr[i];
+
+    }
+    cout<<"array is : ";
+    for(i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+    cout<<quick(arr,0,n-1);
+}
